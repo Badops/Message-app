661b14197ebe6ea5685d3b4b84caeefdafcee9bc
Badmos Opeyemi <opeyemi.badmos@gmail.com>
2018-03-31 22:15:39 +0100

    create commands_list.ex file

---
+++ commands_list.ex
@@ -0,0 +1,5 @@
+## This file contains all the commands that were done throughout the development of this app
+
+## mix phx.new message_app
+
+## create commands_list.ex file (../message_app/commands_list.ex)
